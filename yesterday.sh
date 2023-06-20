d2ee2b5c3d0957d55f38dcc396bda1d768694492
Ignacio Rodríguez <rocha.iarch@gmail.com>
2023-06-19 21:25:10 -0400

    CHG: Script con formato funcionando en linux

--- yesterday.sh
+++ yesterday.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
 
+## Este script no funciona en MacOS (por la opción -d, próximamente buscaré un sustituto para hacerlo compatible)
+
+FECHA="$(date -d '-1 day' +%Y.%m.%d)"
+echo "La fecha de ayer en formato YYYY.MM.DD es: $FECHA"
